2edb0b7d648f555446515d07cf1d8a8ef41e58db
zvanderbilt <zack@kiosk.tm>
2016-04-12 10:25:11 -0700

    working csv export

--- plugin_iterator.rb
+++ plugin_iterator.rb
@@ -9,7 +9,7 @@ require 'csv'
 
 class WPParser
 
-Version = 1.0
+Version = 1.1
 
 def self.parse(args)
     # The options specified on the command line will be collected in *options*.
@@ -69,32 +69,37 @@ end  # class OptionParser
 def wp_found(options)
     begin
     @options = options
-
+    
     wpconfigs = Array.new()
         Find.find(@options[:target]) do |path|
         wpconfigs << path if path =~ /wp\-config\.php$/
     end
 
     wpconfigs.each do |file|
+        if file =~ /bak/
+            next	
+        end
         name, user, password, host = File.read(file).scan(/'DB_[NAME|USER|PASSWORD|HOST]+'\, '(.*?)'/).flatten
         puts "Getting plugins for..."
-        @site_name
         @site_name = get_site_name(name, user, password, host)
+	puts @site_name
+
+        target_csv = Dir.glob("/tmp/plugins_*").max_by {|f| File.mtime(f)}
 
         @wpcli = Wpcli::Client.new File.dirname(file)
+	#sitename = @wpcli.run "option get siteurl --allow-root"
+	puts "Site name is...#{@site_name}!"
+	CSV.open(target_csv, "a") do |csv|
+            csv << [@site_name,]
+        end
+
         plugins = @wpcli.run "plugin list --allow-root"
         plugins.each do |plugin|
             puts "#{plugin[:name]} is version #{plugin[:version]} and an update is #{plugin[:update]}"
-            @plugin_name = plugin[:name] 
-            @plugin_version = plugin[:version] 
-            @plugin_update = plugin[:update]
+            CSV.open(target_csv, "a") do |csv|
+                csv << ['', plugin[:name], plugin[:version], plugin[:update]]
+            end
         end
-            time = Time.now
-                CSV.open("/tmp/plugins_#{time}.csv", "wb") do |csv|
-                    csv << ["Site name", "Plugin", "Version", "Upgradeable"]
-                    csv << [@site_name,]
-                    csv << ["", @plugin_name, @plugin_version, @plugin_update]
-                end
     end
     rescue => e
         puts e
@@ -105,7 +110,7 @@ def get_site_name(db_name, db_user, db_pass, db_host)
     begin
     con = Mysql.new("#{db_host}", "#{db_user}", "#{db_pass}", "#{db_name}")
     rs = con.query('SELECT option_value FROM wp_options WHERE option_id = 3')
-    puts rs.fetch_row
+    return rs.fetch_row[0]
 
     rescue => e
         puts e
@@ -114,19 +119,15 @@ ensure
     con.close if con
 end
 
-#def generate_csv()
-#    begin
-#    puts @sitename
-#    time = Time.now
-#    CSV.open("/tmp/plugins_#{time}.csv", "wb") do |csv|
-#        csv << ["Site name", "Plugin", "Version", "Upgradeable"]
-#        csv << [@sitename]
-#        csv << ["", "plugin[:name]}", "@#{plugin[:version]}", "@#{plugin[:update]}"]
-#    end
-#    rescue => e
-#        puts e
-#    end
-#end
+def generate_csv()
+    begin
+    CSV.open("/tmp/plugins_#{Time.now}.csv", "a+") do |csv|
+        csv << ["Site name", "Plugin", "Version", "Upgradeable"]
+    end
+    rescue => e
+        puts e
+    end
+end
 
 begin
     options = WPParser.parse(ARGV)
@@ -136,6 +137,7 @@ begin
     sleep 2
 
 # Print db connection info and site name
+    generate_csv()
     puts wp_found(options)
 
 rescue => e
